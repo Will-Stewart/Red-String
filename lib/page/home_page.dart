ca91dec599fba9b5c337fc75660b610ee4abd9c5
Will Stewart <william.stewart@students.plymouth.ac.uk>
2021-12-08 17:30:20 +0000

    Implement list and appbar customizations

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) => const Scaffold(
-    backgroundColor: Color(0xffFEF7F0),
-    body: Center(
-      child: Text('Home Page'),
-    ),
-  );
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Home Page',
+            style: GoogleFonts.nunito(
+                fontWeight: FontWeight.bold,
+                textStyle: const TextStyle(color: Color(0xFF493843)),
+                fontSize: 24),
+          ),
+          backgroundColor: const Color(0xFFEED9BB),
+          shadowColor: const Color(0xFFEED9BB),
+          elevation: 0,
+          centerTitle: true,
+        ),
+        body: ListView(
+          padding: const EdgeInsets.all(16),
+          children: <Widget>[
+            Container(
+                height: 150.0,
+                width: 300.0,
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Color(0xFFF5896E),
+                        Color(0xFFFFB19D),
+                      ],
+                    )),
+                child: const Center(
+                  child: Text(
+                    "Widget",
+                    style: TextStyle(color: Colors.white, fontSize: 22),
+                    textAlign: TextAlign.center,
+                  ),
+                )),
+          ],
+        ),
+      );
 }
