250da9427a9d77a863778527c9e46a77d86ef652
Will Stewart <william.stewart@students.plymouth.ac.uk>
2021-12-08 19:57:02 +0000

    update profile page

--- lib/page/messages_page.dart
+++ lib/page/messages_page.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MessagesPage extends StatelessWidget {
+  const MessagesPage({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) => const Scaffold(
-        backgroundColor: Color(0xffFEF7F0),
-        body: Center(
-          child: Text('Messages Page'),
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(30),
+          )),
+          title: Text(
+            'Messages Page',
+            style: GoogleFonts.nunito(
+                fontWeight: FontWeight.bold,
+                textStyle: const TextStyle(color: Color(0xFF493843)),
+                fontSize: 24),
+          ),
+          backgroundColor: const Color(0xFFEED9BB),
+          shadowColor: const Color(0xFFEED9BB),
+          elevation: 0,
+          centerTitle: true,
         ),
       );
 }
--- lib/page/profile_page.dart
+++ lib/page/profile_page.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) => const Scaffold(
-        backgroundColor: Color(0xffFEF7F0),
-        body: Center(
-          child: Text('Profile Page'),
+  Widget build(BuildContext context) => Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(30),
+          )),
+          title: Text(
+            'Profile Page',
+            style: GoogleFonts.nunito(
+                fontWeight: FontWeight.bold,
+                textStyle: const TextStyle(color: Color(0xFF493843)),
+                fontSize: 24),
+          ),
+          backgroundColor: const Color(0xFFEED9BB),
+          shadowColor: const Color(0xFFEED9BB),
+          elevation: 0,
+          centerTitle: true,
         ),
+        body: Stack(children: <Widget>[
+          Container(
+            height: 300.0,
+            decoration: BoxDecoration(
+              gradient: const LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color(0xFF493843),
+                  Color(0xFF2b2128),
+                ],
+              ),
+              borderRadius: BorderRadius.vertical(
+                  bottom: Radius.elliptical(
+                      MediaQuery.of(context).size.width, 200.0)),
+            ),
+          ),
+        ]),
       );
 }
